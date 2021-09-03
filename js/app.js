c418f40cb794c4089444857e02019110fcd2b12d
jayeen29 <jayeen6783@gmail.com>
2021-09-03 18:26:02 +0600

    show icon system added

--- js/app.js
+++ js/app.js
@@ -4,8 +4,9 @@ const getId = elementId => {
 };
 //load data
 const loadData = () => {
-    const countryInput = document.getElementById('country-input').value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${countryInput}&appid=a5ec2e8f85653d333815f8ca2fffdf28`;
+    const countryInput = document.getElementById('country-input');
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${countryInput.value}&appid=a5ec2e8f85653d333815f8ca2fffdf28`;
+    countryInput.value = '';
     fetch(url)
         .then(res => res.json())
         .then(data => showDataUi(data));
@@ -16,4 +17,6 @@ const showDataUi = data => {
     const calculateTemp = Math.ceil(data.main.temp / 10);
     getId('country-temperature').innerText = calculateTemp;
     getId('weather-condition').innerText = data.weather[0].main;
+    const url = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    getId('weather-icon').src = url;
 };
